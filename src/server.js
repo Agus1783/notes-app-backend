107484d6aac74306bd8a87e2068f8fb5a31ab575
unknown <17agus03@gmail.com>
2026-04-24 03:36:21 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+// Memuat kode untuk membuat, mengonfigurasi, dan menjalankan server HTTP menggunakan Express
+import express from 'express';
+import routes from './routes.js';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Aplikasi berjalan di http://${host}:${port}`);
+});
+
