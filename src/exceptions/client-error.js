4767735b6106141ae9893013b0698fca4e23c577
Regoel <17agus03@gmail.com>
2026-05-04 07:06:34 +0700

    Fix string quotes in ClientError class

--- src/exceptions/client-error.js
+++ src/exceptions/client-error.js
@@ -3,7 +3,7 @@
 class ClientError extends Error {
   constructor(message, statusCode = 400) {
     super(message);
-    this.name = 'ClientError";
+    this.name = "ClientError";
     this.statusCode = statusCode;
   }
 }
