ccee466892fceba83bf4a53452e8ce37e6f03b97
unknown <17agus03@gmail.com>
2026-05-05 16:42:52 +0700

    menghubungkan ke postgres dan install config dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 // Memuat kode untuk membuat, mengonfigurasi, dan menjalankan server HTTP menggunakan Express
-import server from './server/index.js';
- 
-const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
-const port = process.env.PORT || 3000;
- 
+import server from "./server/index.js";
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
 server.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
 });
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 //menyiapkan server Express agar bisa menerima data JSON
 //dan mengatur alamat (endpoint) API secara rapi dan terpisah.
+import "dotenv/config";
 import express from "express";
 import routes from "../routes/index.js";
 import ErrorHandler from "../middlewares/error.js";
