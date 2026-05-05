9ec893462522514041a6960b351f90a8e407b426
unknown <17agus03@gmail.com>
2026-05-05 20:58:01 +0700

    implementasi database postgres

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -4,8 +4,8 @@ import {
   createNote,
   getNotes,
   getNoteById,
-  editNoteById,
-  deleteNoteById,
+  editNote,
+  deleteNote,
 } from "../controller/note-controller.js";
 import validate from "../../../middlewares/validate.js";
 import validateQuery from "../../../middlewares/validateQuery.js";
@@ -17,6 +17,6 @@ const router = express.Router();
 router.post("/notes", validate(notePayloadSchema), createNote);
 router.get("/notes", validateQuery(noteQuerySchema), getNotes);
 router.get("/notes/:id", getNoteById);
-router.put("/notes/:id", validate(notePayloadSchema), editNoteById);
-router.delete("/notes/:id", deleteNoteById);
+router.put("/notes/:id", validate(notePayloadSchema), editNote);
+router.delete("/notes/:id", deleteNote);
 export default router;
