5bfad7dac8278a0ea934ef123e5400b3067c2ef1
unknown <17agus03@gmail.com>
2026-05-08 21:29:18 +0700

    menambahkan routes users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 // digunakan untuk mendaftarkan route milik notes yang ada di services/notes/routes.
-import { Router } from 'express';
-import notes from '../services/notes/routes/index.js';
- 
+import { Router } from "express";
+import notes from "../services/notes/routes/index.js";
+import users from "../services/users/routes/index.js";
+
 const router = Router();
- 
-router.use('/', notes);
- 
+
+router.use("/", notes);
+router.use("/", users);
+
 export default router;
