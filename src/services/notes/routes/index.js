cf40c3da3e9766fe85aaa117aa407e413888ef6e
unknown <17agus03@gmail.com>
2026-05-10 13:04:35 +0700

    menyelesaikan authentication

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -13,10 +13,26 @@ import {
   notePayloadSchema,
   noteQuerySchema,
 } from "../../../services/notes/validator/schema.js";
+import authenticateToken from "../../../middlewares/auth.js";
 const router = express.Router();
-router.post("/notes", validate(notePayloadSchema), createNote);
-router.get("/notes", validateQuery(noteQuerySchema), getNotes);
-router.get("/notes/:id", getNoteById);
-router.put("/notes/:id", validate(notePayloadSchema), editNote);
-router.delete("/notes/:id", deleteNote);
+router.post(
+  "/notes",
+  authenticateToken,
+  validate(notePayloadSchema),
+  createNote,
+);
+router.get(
+  "/notes",
+  authenticateToken,
+  validateQuery(noteQuerySchema),
+  getNotes,
+);
+router.get("/notes/:id", authenticateToken, getNoteById);
+router.put(
+  "/notes/:id",
+  authenticateToken,
+  validate(notePayloadSchema),
+  editNote,
+);
+router.delete("/notes/:id", authenticateToken, deleteNote);
 export default router;
